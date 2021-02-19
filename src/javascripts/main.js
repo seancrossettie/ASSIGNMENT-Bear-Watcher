431d4d089ca32ff439438a7c516f5aa6f72a8e97
Sean <jb@JBs-MacBook-Pro.local>
2021-02-18 19:06:49 -0600

    created development branch

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,31 @@
 import '../styles/main.scss';
 
+// const bearArray = [];
+
+const printToDom = (divId, domString) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = domString;
+};
+
+const createForm = () => {
+  const formString = `<div class="jumbotron"">
+                        <form>
+                          <div class="mb-3">
+                            <label for="newBearName" class="form-label">First name</label>
+                            <input type="text" class="form-control" id="newBearName" placeholder="URL" required>
+                          </div>
+                          <div class="mb-3">
+                            <label for="imgURL" class="form-label">New Bear</label>
+                            <input type="url" class="form-control" id="newImgUrl" placeholder="URL" required>
+                          </div>
+                            <button type="button" class="btn btn-light" id="createBearBtn">Track Bear</button>
+                        </form>
+                      </div>`;
+  printToDom('#form', formString);
+};
+
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  createForm();
 };
 
 init();
